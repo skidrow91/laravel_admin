82ea4e1162aa7bafccbc836d52fe90f92b8626ce
at-hanlv <hanlv@asiantech.vn>
2017-04-03 17:26:59 +0700

    first commit

---
+++ app/Http/Controllers/AssminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class AssminController extends Controller{
+
+    public function index(){
+        return view('backend.index');
+    }
+
+    public function login(){
+        return view('backend.login');
+    }
+
+    public function signup(){
+        return view('backend.signup');
+    }
+}
